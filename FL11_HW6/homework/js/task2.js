5fa66b304a30464fabc1b1db19705008616a0b7a
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-07-04 23:33:27 +0300

    Commit HW-6 v.2

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,6 +1,6 @@
-let a = +prompt('Enter size A');
-let b = +prompt('Enter size B');
-let c = +prompt('Enter size C');
+let a = +prompt('Enter lenght of side A');
+let b = +prompt('Enter lenght of side B');
+let c = +prompt('Enter lenght of side C');
 if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a) {
     if (a === b && a === c && b === c) {
         console.log('Eequivalent triangle');
